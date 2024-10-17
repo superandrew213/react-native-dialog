531681cdfcf0af34aa8e528b54a32c75bbf3ad43
Andrew Shini <13059204+superandrew213@users.noreply.github.com>
2024-10-18 09:13:06 +1000

    use runOnUiThread

--- android/src/main/java/com/honaf/dialog/MyDialogModule.java
+++ android/src/main/java/com/honaf/dialog/MyDialogModule.java
@@ -1,11 +1,9 @@
 package com.honaf.dialog;
 
-
-
 import android.app.Activity;
 import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.AppCompatEditText;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.AppCompatEditText;
 import android.view.ViewGroup;
 import android.view.WindowManager.LayoutParams;
 import android.widget.LinearLayout;
@@ -18,114 +16,108 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 
-
-/**
- * Created by honaf on 16/2/29.
- * dialog
- */
-public class MyDialogModule extends ReactContextBaseJavaModule
-{
+public class MyDialogModule extends ReactContextBaseJavaModule {
     public ReactContext currentReactContext = null;
+
     public MyDialogModule(ReactApplicationContext reactContext) {
         super(reactContext);
         currentReactContext = reactContext;
     }
+
     @Override
     public String getName() {
         return "MyDialogModule";
     }
 
-    /**
-     * 弹出类似于相册
-     * @param array
-     * @param callback
-     */
     @ReactMethod
-    public void actionSheet(ReadableArray array, final Callback callback){
-        final String[] str = new String[array.size()];
-        for (int i = 0; i<array.size(); i++ ){
-            str[i] = array.getString(i);
-        }
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this.getCurrentActivity());
-        dialog.setItems(str, new DialogInterface.OnClickListener() {
+    public void actionSheet(final ReadableArray array, final Callback callback) {
+        final Activity activity = getCurrentActivity();
+        if (activity == null) return;
+
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                final String[] str = new String[array.size()];
+                for (int i = 0; i < array.size(); i++) {
+                    str[i] = array.getString(i);
+                }
+                AlertDialog.Builder dialog = new AlertDialog.Builder(activity);
+                dialog.setItems(str, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         callback.invoke(i);
                     }
-                }
-        );
-        dialog.show();
+                });
+                dialog.show();
+            }
+        });
     }
-    /**
-     * 编辑弹出框
-     */
+
     @ReactMethod
-    public  void inputDialog(ReadableMap map,final Callback callback1,final Callback callback2) {
-            if(map == null){
-                return;
-            }
-            Activity context = this.getCurrentActivity();
-            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
-            dialogBuilder.setTitle(map.getString("title"));
-            if (map.hasKey("message")) {
-              dialogBuilder.setMessage(map.getString("message"));
-            }
-            final AppCompatEditText editText = new AppCompatEditText(context);
-            editText.setText(map.hasKey("content") ? map.getString("content") : "");
-            editText.setSingleLine(true);
-            editText.setBackgroundResource(R.drawable.bg_home_search);
-            editText.requestFocus();
-            editText.setPadding(10, 8,
-                    10, 8);
-            editText.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            LinearLayout layout = new LinearLayout(context);
-            layout.setPadding(15, 10,
-                    15, 0);
-            layout.addView(editText);
-            dialogBuilder.setView(layout);
+    public void inputDialog(final ReadableMap map, final Callback callback1, final Callback callback2) {
+        final Activity activity = getCurrentActivity();
+        if (activity == null) return;
 
-            ReadableArray array = map.getArray("callbackOrButtons");
-            String leftBtn = "确定";
-            String rightBtn = "取消";
-            if(array == null){
-                return;
-            }else if(array.size() == 1){
-                leftBtn = array.getMap(0).getString("text");
-            }else if(array.size() == 2){
-                leftBtn = array.getMap(0).getString("text");
-                rightBtn = array.getMap(1).getString("text");
-            }
-            dialogBuilder.setPositiveButton(leftBtn, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    String text = editText.getText().toString().trim();
-                    callback1.invoke(text);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (map == null) {
+                    return;
                 }
-            });
-            dialogBuilder.setNegativeButton(rightBtn, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    String text = editText.getText().toString().trim();
-                    if (callback2 != null) {
-                        callback2.invoke(text);
+
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity);
+                dialogBuilder.setTitle(map.getString("title"));
+                if (map.hasKey("message")) {
+                    dialogBuilder.setMessage(map.getString("message"));
+                }
+
+                final AppCompatEditText editText = new AppCompatEditText(activity);
+                editText.setText(map.hasKey("content") ? map.getString("content") : "");
+                editText.setSingleLine(true);
+                editText.setBackgroundResource(R.drawable.bg_home_search);
+                editText.requestFocus();
+                editText.setPadding(10, 8, 10, 8);
+                editText.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                LinearLayout layout = new LinearLayout(activity);
+                layout.setPadding(15, 10, 15, 0);
+                layout.addView(editText);
+                dialogBuilder.setView(layout);
+
+                ReadableArray array = map.getArray("callbackOrButtons");
+                String leftBtn = "确定";
+                String rightBtn = "取消";
+                if (array == null) {
+                    return;
+                } else if (array.size() == 1) {
+                    leftBtn = array.getMap(0).getString("text");
+                } else if (array.size() == 2) {
+                    leftBtn = array.getMap(0).getString("text");
+                    rightBtn = array.getMap(1).getString("text");
+                }
+
+                dialogBuilder.setPositiveButton(leftBtn, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String text = editText.getText().toString().trim();
+                        callback1.invoke(text);
                     }
-                }
-            });
-            AlertDialog dialog = dialogBuilder.create();
-            dialog.getWindow().setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-            dialog.show();
+                });
+
+                dialogBuilder.setNegativeButton(rightBtn, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String text = editText.getText().toString().trim();
+                        if (callback2 != null) {
+                            callback2.invoke(text);
+                        }
+                    }
+                });
+
+                AlertDialog dialog = dialogBuilder.create();
+                dialog.getWindow().setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                dialog.show();
+            }
+        });
     }
-
-
-//      @ReactMethod -> JavaScript
-//    Boolean -> Bool
-//    Integer -> Number
-//    Double -> Number
-//    Float -> Number
-//    String -> String
-//    Callback -> function
-//    ReadableMap -> Object
-//    ReadableArray -> Array
-
-
 }
